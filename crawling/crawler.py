2b5adceeacc32cd2b14a69b4eab46fa0e63949a8
chojaehyeok99 <choco5366@naver.com>
2024-03-05 17:01:49 +0900

    2024 - 03 - 05 python 크롤링(3) : 웹 크롤링(2)

--- crawling/crawler.py
+++ crawling/crawler.py
@@ -38,19 +38,66 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome()
-time.sleep(3)
+time.sleep(2)
 
 driver.get(URL)
-time.sleep(3)
+time.sleep(2)
 
 search_input = driver.find_element(By.ID, 'query')
 search_input.send_keys('제네시스')
-time.sleep(3)
+time.sleep(2)
 search_input.send_keys(Keys.ENTER)
-time.sleep(3)
+time.sleep(2)
 
 news_button = driver.find_element(By.CSS_SELECTOR, '#lnb > div.lnb_group > div > div.lnb_nav_area._nav_area_root > div > div.api_flicking_wrap._conveyer_root > div:nth-child(8) > a')
 time.sleep(1)
 
 ActionChains(driver).click(news_button).perform()
-time.sleep(5)
+time.sleep(2)
+
+news_title_elements = driver.find_elements(By.CLASS_NAME, 'news_tit')
+time.sleep(1)
+
+for news_title_element in news_title_elements :
+    #title = news_title_element.text
+    title = news_title_element.get_attribute('title')
+    print(title)
+time.sleep(1)
+
+driver.back()
+time.sleep(2)
+
+image_button = driver.find_element(By.CSS_SELECTOR, '#lnb > div.lnb_group > div > div.lnb_nav_area._nav_area_root > div > div.api_flicking_wrap._conveyer_root > div:nth-child(1) > a')
+time.sleep(1)
+
+ActionChains(driver).click(image_button).perform()
+time.sleep(2)
+
+images_elements = driver.find_elements(By.CLASS_NAME, '_fe_image_tab_content_thumbnail_image')
+time.sleep(1)
+
+image_list = []
+
+for images_element in images_elements :
+    image_src = images_element.get_attribute('src')
+    image_list.append(image_src)
+time.sleep(1)
+#-------------------------------------------
+# 파이썬으로 폴더 생성
+import os
+
+FOLDER_PATH = r'./images/'
+
+if not os.path.isdir(FOLDER_PATH):
+    os.mkdir(FOLDER_PATH)
+#--------------------------------------------
+# 파이썬으로 이미지 url 파일 다운로드 방법
+from urllib.request import urlretrieve
+
+number = 1
+
+for image_src in image_list :
+    urlretrieve(image_src, FOLDER_PATH + f'{number}.png')
+    number += 1
+    time.sleep(0.5)
+#--------------------------------------------
